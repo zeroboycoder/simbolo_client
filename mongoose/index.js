62d454e51713f376615cd8238bb6d88837bd5d23
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2023-01-12 18:42:17 +0630

    Change cloud monngo uri

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27017/simbolo";
+// const uri = "mongodb://localhost:27017/simbolo";
+const uri = "mongodb+srv://pyaesonekhant:Pyaesonekhant27@cluster0.fsiexts.mongodb.net/simbolo?retryWrites=true&w=majority";
 
 const connectDB = () => {
     mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
